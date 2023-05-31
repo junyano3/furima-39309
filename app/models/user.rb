3231b53bb21dfb8194a9901705749c1b8553125e
junyano3 <sonecchi1117@gmail.com>
2023-05-31 12:21:36 +0900

    新規登録機能
    デバイス導入から新規登録機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+      validates :email, presence: true
+      validates :encrypted_password, presence: true    
+      validates :nickname, presence: true
+
+      with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+      validates :first_name, presence: true
+      validates :last_name, presence: true
+      end
+      with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カナ文字を使用してください' } do
+      validates :first_name_kana, presence: true
+      validates :last_name_kana, presence: true
+      end
+
+      validates :date_of_birth , presence: true
+
+      has_many :items
+      has_many :purchase_records
+    end
