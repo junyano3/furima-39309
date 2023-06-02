837cb40d964b902676bab58959a9a393d81d1cd4
junyano3 <sonecchi1117@gmail.com>
2023-06-02 16:54:03 +0900

    画像投稿機能導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,7 @@ end
 
 private
 def item_params
-  params.require(:item).permit(:item_name, :product_description, :item_price, :category_id, :condition_id, :shipping_fee_responsibility_id, :prefecture_id, :shipping_duration_id, ).merge(user_id: current_user.id)
+  params.require(:item).permit(:image,:item_name, :product_description, :item_price, :category_id, :condition_id, :shipping_fee_responsibility_id, :prefecture_id, :shipping_duration_id, ).merge(user_id: current_user.id)
 end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,12 @@ class Item < ApplicationRecord
   # has_one  :purchase_record
 
  belongs_to :user
-
+ has_one_attached :image
  extend ActiveHash::Associations::ActiveRecordExtensions
  belongs_to :category
 
  validates :item_name, :product_description, :item_price,presence: true
  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  
+
 
 end
