3732a7b2c76e280bc76fdafa052447f99df49e27
junyano3 <sonecchi1117@gmail.com>
2023-06-05 23:43:57 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
 root to: "items#index"
 resources :users, only: [:edit, :update,:destroy]
-resources :items, only: [:index, :new, :create]
+resources :items, only: [:index, :new, :create, :show ]
 
 
 end
