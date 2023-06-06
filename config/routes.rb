fb135811fb25bc56a4ff8655c91431ce6f32c5aa
junyano3 <sonecchi1117@gmail.com>
2023-06-06 17:44:16 +0900

    商品削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 root to: "items#index"
 resources :users, only: [:edit, :update,:destroy]
 
-resources :items, only: [:index, :new, :create,:show,:edit,:update]
+resources :items
 
 end
