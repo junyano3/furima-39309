9f5185891f6126587673ef3632bd0d8fa04ebb8f
junyano3 <sonecchi1117@gmail.com>
2023-06-09 15:23:29 +0900

    購入内容の表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,10 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
   def index
-
+  @item = Item.find(params[:item_id])
   end
+
   def create
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
 root to: "items#index"
 resources :users, only: [:edit, :update,:destroy]
 
-resources :items
-
+resources :items do
 resources :orders, only: [:index, :new, :create]
-
+end
 end
